63fbac32f44b3b0e3f5be3771c9b6e29591a6633
bebtio <bebtio@gmail.com>
2021-01-09 19:07:00 -0700

    AJH: added comments

--- 4_olcMMBNMap/SpriteAnimation.cpp
+++ 4_olcMMBNMap/SpriteAnimation.cpp
@@ -14,7 +14,12 @@ SpriteAnimation::SpriteAnimation( std::string spritePath )
 }
 
 //**************************************************************//
-// addFrame
+// addFrame - Adds a frame of animation to this Animation.
+// pulls pixels from framePos.x to framDims.x and
+// framePos.y to frameDims.y.
+//
+// framePos - the position in the sprite sheet this frame starts at.
+// frameDims - the size, height and width, of the frame.
 //**************************************************************//
 void SpriteAnimation::addFrame( olc::vi2d framePos, olc::vi2d frameDims )
 {
@@ -25,23 +30,35 @@ void SpriteAnimation::addFrame( olc::vi2d framePos, olc::vi2d frameDims )
  
 //**************************************************************//
 // addFrame
+// Same as above but used the uses each x and y as an input instead
+// of as a pair of olc::vi2d's. Ends up calling the addFrame function
+// above.
 //**************************************************************//
 void SpriteAnimation::addFrame( uint32_t xFramePos, uint32_t yFramePos, uint32_t xFrameDim, uint32_t yFrameDim )
 {
-    this->framePos. push_back( olc::vi2d( xFramePos, yFramePos ) );
-    this->frameDims.push_back( olc::vi2d( xFrameDim, yFrameDim ) );
-    numFrames++;
+    addFrame( olc::vi2d( xFramePos, yFramePos ), olc::vi2d( xFrameDim, yFrameDim ) );
 }
 
+//**************************************************************//
+// addFrameCornerCoords
+// Same as above but used uses the upper left and lower right
+// corner coordinates of the desired frame to load the sprite.
+// This is sometimes easier than calculating the height and width
+// of the frame yourself.
+//**************************************************************//
 void SpriteAnimation::addFrameCornerCoords( olc::vi2d topLeftCorner, olc::vi2d bottomRightCorner )
 {
+    // Compute the width and height of the frame.
     uint32_t xFrameDim( bottomRightCorner.x - topLeftCorner.x );
     uint32_t yFrameDim( bottomRightCorner.y - topLeftCorner.y );
 
+    // Create a vi2d object.
     olc::vi2d frameDims( xFrameDim, yFrameDim );
 
+    // Call the other addFrame function.
     addFrame( topLeftCorner, frameDims );
 }
+
 //**************************************************************//
 // Loops over frames. Resets currFrame to 0 when we exceed the 
 // number of frames this animation has.
@@ -49,14 +66,22 @@ void SpriteAnimation::addFrameCornerCoords( olc::vi2d topLeftCorner, olc::vi2d b
 void SpriteAnimation::update()
 {
     currFrame++;
-    if( currFrame >= numFrames )
+    // Minus 1 for zero indexing.
+    if( currFrame > numFrames - 1 ) 
     {
         currFrame = 0;
     }
 }
 
+void SpriteAnimation::setFrameNum( uint32_t frameNum )
+{
+    // Minus 1 for zero indexing. Clamp to realistic ranges.
+    if( frameNum > numFrames - 1 ) { frameNum = numFrames; }
+    if( frameNum < 0 )             { frameNum = 0;         }
+}
+
 //**************************************************************//
-//
+// Draws the sprite at the to screen at position drawPos.
 //**************************************************************//
 void SpriteAnimation::drawSelf( olc::vi2d drawPos )
 {
@@ -64,7 +89,7 @@ void SpriteAnimation::drawSelf( olc::vi2d drawPos )
 }
 
 //**************************************************************//
-//
+// Draws the sprite to the screen at the position (xDrawPos,yDrawPos).
 //**************************************************************//
 void SpriteAnimation::drawSelf( uint32_t xDrawPos, uint32_t yDrawPos )
 {
@@ -77,12 +102,11 @@ void SpriteAnimation::drawSelf( uint32_t xDrawPos, uint32_t yDrawPos )
         throw;
     }
 
-    if( pge->GetKey( olc::N).bPressed ) 
-    { update(); }
+    if( pge->GetKey( olc::N).bPressed ) { update(); }
 
-    olc::vi2d currPos ( framePos.at( currFrame ) );
-    olc::vi2d currDims( frameDims.at(currFrame ) );
-    olc::vi2d drawPos ( xDrawPos, yDrawPos       );
+    olc::vi2d currPos ( framePos.at ( currFrame ) );
+    olc::vi2d currDims( frameDims.at( currFrame ) );
+    olc::vi2d drawPos ( xDrawPos, yDrawPos        );
 
     pge->DrawPartialSprite( drawPos, sprite, currPos, currDims );
 
--- 4_olcMMBNMap/SpriteAnimation.hpp
+++ 4_olcMMBNMap/SpriteAnimation.hpp
@@ -6,6 +6,10 @@
 #include <iostream>
 #include <string>
 
+
+//****************************************************//
+//
+//****************************************************//
 class SpriteAnimation : public olc::PGEX
 {
 
@@ -16,17 +20,22 @@ public:
     // Add a new frame to this animation.
     void addFrame( olc::vi2d framePos, olc::vi2d frameDims );
     void addFrame( uint32_t xFramePos, uint32_t yFramePos, uint32_t xFrameDim, uint32_t yFrameDim );
+
+    // Use the corner coordinates of the frame within the sprite sheet to add instead.
     void addFrameCornerCoords( olc::vi2d topLeftCorner, olc::vi2d bottomRightCorner );
 
     void update();
+    void setFrameNum( uint32_t frameNum = 0 );
     void drawSelf( olc::vi2d drawPos );
     void drawSelf( uint32_t xDrawPos, uint32_t yDrawPos );
 
     const olc::vi2d getCurrentDims() { return( frameDims.at( currFrame ) ); }
 private:
+
     olc::Sprite* sprite;
     std::vector<olc::vi2d> framePos;
     std::vector<olc::vi2d> frameDims;
+
     uint32_t numFrames;
     uint32_t currFrame;
 };
