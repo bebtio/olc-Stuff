62c7d4b9aa31d10af5d161581d441a3b46b5cbe7
bebtio <bebtio@gmail.com>
2021-08-01 08:34:12 -0700

    AJH: got it running with olc. Forgot you needed the 'define OLC_PGE_APPLICATION so that it actually compiles correctly.

--- 5_olcRecursion/main.cpp
+++ 5_olcRecursion/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include "RecursiveDisplay.hpp"
 
 int main( int argc, char* argv[] )
 {
 
+    RecursiveDisplay display;
+
+    display.Construct(240,160,2,2);
+    display.Start();
+    
     std::cout << "Hello world" << std::endl;
 
     return(0);
